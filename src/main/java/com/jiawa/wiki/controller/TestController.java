2dfb032e861462ae42232a06dd3ad8cba569fac7
寿富秋 <1060037207@qq.com>
2023-07-17 18:01:22 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.jiawa.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController//返回字符串
@@ -11,8 +11,12 @@ public class TestController {
      * GET,POST,PUT,DELETE分别对应查询，新增，修改，删除
      * @return
      */
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return ("Hello World,"+name);
     }
 }
