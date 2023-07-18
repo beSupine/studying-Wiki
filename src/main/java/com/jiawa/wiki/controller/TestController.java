f3f9d024d709f4f2155d26a237bf62ec20cca961
寿富秋 <1060037207@qq.com>
2023-07-18 10:28:05 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController//返回字符串
 //@Controller//返回页面
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private String testHello;
     /**
      * GET,POST,PUT,DELETE分别对应查询，新增，修改，删除
      * @return
      */
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!"+testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name) {
