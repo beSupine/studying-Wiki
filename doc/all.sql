b9e9519b7fa58c00e97b8252d5e4f7025b5fbdfe
寿富秋 <1060037207@qq.com>
2023-07-23 16:41:01 +0800

    3-5 集成mabatis generator,生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -12,3 +12,13 @@ create table `test`
 
 insert into `test` (id, name, password) values (1,'测试','password');
 select * from `test`;
+
+drop table if exists `demo`;
+create table `demo`
+(
+    `id`       bigint not null comment 'id',
+    `name`     varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment = '测试';
+insert into `demo` (id, name) values (1,'测试');
