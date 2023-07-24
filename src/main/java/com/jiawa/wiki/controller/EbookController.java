ad734a240469e3e9d05b6240deace22e1d6e06f4
寿富秋 <1060037207@qq.com>
2023-07-24 11:13:26 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -0,0 +1,26 @@
+package com.jiawa.wiki.controller;
+
+import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController//返回字符串
+@RequestMapping("/ebook")
+//@Controller//返回页面
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+    /**
+     * GET,POST,PUT,DELETE分别对应查询，新增，修改，删除
+     */
+    @GetMapping("/list")
+    public List<Ebook> list() {
+        return ebookService.list();
+    }
+}
