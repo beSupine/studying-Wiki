18c0870fbda8a90eaeaf220490cdc6e53890da20
寿富秋 <1060037207@qq.com>
2023-09-05 17:21:58 +0800

    5-4 列表查询接口支持动态SQL name参数有值时按name查，没值时查全部 封面小图样式调整

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -102,7 +102,7 @@ export default defineComponent({
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
     onMounted(() => {
-      axios.get("http://localhost:8880/ebook/list?name=Vue").then((response) => {
+      axios.get("http://localhost:8880/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
@@ -120,3 +120,13 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+.ant-avatar {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
+</style>
