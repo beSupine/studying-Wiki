3a78ae6169e1afeb0a180b2a7ab8c3e0dbbd2f4e
beSupine <shoufuqiu@outlook.com>
2023-09-14 20:06:12 +0800

    6-9 增加雪花算法工具类，完成电子书是新增功能。 数据库*_count值设置成可以为null。

--- src/main/java/com/jiawa/wiki/resp/EbookQueryResp.java
+++ src/main/java/com/jiawa/wiki/resp/EbookQueryResp.java
@@ -1,6 +1,6 @@
 package com.jiawa.wiki.resp;
 
-public class EbookQueryResp {
+public class  EbookQueryResp {
     private Long id;
 
     private String name;
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,6 +3,11 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <p>
+        <a-button type="primary" @click="add()" size="large">
+          新增
+        </a-button>
+      </p>
       <a-table
           :columns="columns"
           :row-key="record => record.id"
@@ -168,6 +173,13 @@ export default defineComponent({
       modalVisible.value = true;
       ebook.value = record
     };
+    /**
+     * 新增
+     */
+    const add = () => {
+      modalVisible.value = true;
+      ebook.value = {};
+    };
 
     onMounted(() => {
       handleQuery({
@@ -182,7 +194,10 @@ export default defineComponent({
       columns,
       loading,
       handleTableChange,
+
       edit,
+      add,
+
       ebook,
       modalVisible,
       modalLoading,
