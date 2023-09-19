0163c03fb864e88fd709d49903e453e874db6bc6
beSupine <shoufuqiu@outlook.com>
2023-09-19 21:17:06 +0800

    7-8 默认显示欢迎页面，点击欢迎菜单时，显示欢迎页面，点击分类菜单时，显示电子书，只有二级分类才能除法click事件

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -7,10 +7,8 @@
           @click="handleClick"
       >
         <a-menu-item key="welcome">
-          <router-link :to="'/'">
             <MailOutlined/>
             <span>欢迎</span>
-          </router-link>
         </a-menu-item>
         <a-sub-menu v-for="item in level1" :key="item.id">
           <template v-slot:title>
@@ -26,7 +24,10 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+      <div class="welcome" v-show="isShowWelcome">
+        <h1>欢迎使用java知识库</h1>
+      </div>
+      <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
 
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
@@ -101,10 +102,12 @@ export default defineComponent({
         }
       });
     };
-
+    const isShowWelcome = ref(true);
     const handleClick = (value: any) => {
-      console.log("menu click")
+      //console.log("menu click",value)
+      isShowWelcome.value = value.key === 'welcome';
     };
+
 
     onMounted(() => {
       handleQueryCategory();
@@ -127,6 +130,7 @@ export default defineComponent({
 
       handleClick,
       level1,
+      isShowWelcome
 
     }
 
