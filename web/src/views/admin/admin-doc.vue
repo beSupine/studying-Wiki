4d52edc522bed632c7766abd1bd80ded02459482
beSupine <shoufuqiu@outlook.com>
2023-09-21 15:34:36 +0800

    8-6 删除某个文档时，将其下的所有的文档全部删除

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -213,6 +213,39 @@ export default defineComponent({
         }
       }
     };
+    /**
+     * 查找整根树枝
+     */
+    const ids:Array<string> = [];
+    const getDeleteIds = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("disabled", node);
+          // 将目标节点设置为disabled
+          //node.disabled = true;
+          //将目标ID放入结果集ids
+          ids.push(id);
+
+          // 遍历所有子节点
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              getDeleteIds(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            getDeleteIds(children, id);
+          }
+        }
+      }
+    };
 
 
     /**
@@ -245,7 +278,11 @@ export default defineComponent({
      * 删除
      */
     const handleDelete = (id: number) => {
-      axios.delete("/doc/delete/" + id).then((response) => {
+      console.log(1111,level1, level1.value, id)
+      //清空数组，否则多次删除时，数组会一直增加
+      ids.length = 0;
+      getDeleteIds(level1.value, id);
+      axios.delete("/doc/delete/" + ids.join(",")).then((response) => {
         const data = response.data//data = commonResp
         if (data.success) {
           //重新加载列表
