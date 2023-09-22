90daa2cd6837be1ac4a43edece0c121db08351e3
beSupine <shoufuqiu@outlook.com>
2023-09-22 09:29:12 +0800

    8-9 文档管理布局美化 表格紧凑 按钮变小 表单变大 页面打开时就初始化富文本框，z-index变小

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -3,7 +3,7 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-row>
+      <a-row :gutter="24">
         <a-col :span="8">
           <p>
             <a-form layout="inline" :model="param">
@@ -25,6 +25,7 @@
               :data-source="level1"
               :loading="loading"
               :pagination="false"
+              size="small"
           >
             <!--        <template #cover="{ text: cover }">
                       <img v-if="cover" :src="cover" alt="avatar"/>
@@ -34,7 +35,7 @@
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="edit(record)">
+                <a-button type="primary" @click="edit(record)" size="small">
                   编辑
                 </a-button>
                 <a-popconfirm
@@ -43,7 +44,7 @@
                     cancel-text="否"
                     @confirm="handleDelete(record.id)"
                 >
-                  <a-button type="danger">
+                  <a-button type="danger" size="small">
                     删除
                   </a-button>
                 </a-popconfirm>
@@ -52,12 +53,21 @@
           </a-table>
         </a-col>
         <a-col :span="16">
+          <p>
+            <a-form layout="inline" :model="param">
+              <a-form-item>
+                <a-button type="primary" @click="handleSave()">
+                  保存
+                </a-button>
+              </a-form-item>
+            </a-form>
+          </p>
 
-          <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-            <a-form-item label="名称">
-              <a-input v-model:value="doc.name"/>
+          <a-form :model="doc" layout="vertical">
+            <a-form-item>
+              <a-input v-model:value="doc.name" placeholder="名称"/>
             </a-form-item>
-            <a-form-item label="父文档">
+            <a-form-item>
               <a-tree-select
                   v-model:value="doc.parent"
                   style="width: 100%"
@@ -69,10 +79,10 @@
               >
               </a-tree-select>
             </a-form-item>
-            <a-form-item label="顺序">
-              <a-input v-model:value="doc.sort"/>
+            <a-form-item>
+              <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
-            <a-form-item label="内容">
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
@@ -142,17 +152,8 @@ export default defineComponent({
     const columns = [
       {
         title: '名称',
-        dataIndex: 'name'
-      },
-      {
-        title: '父文档',
-        key: 'parent',
-        dataIndex: 'parent'
-      },
-      {
-        title: '顺序',
-        key: 'sort',
-        dataIndex: 'sort'
+        dataIndex: 'name',
+        slots: {customRender: 'name'}
       },
       {
         title: 'Action',
@@ -193,7 +194,8 @@ export default defineComponent({
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const editor = new E('#content');
-    const handleModalOk = () => {
+    editor.config.zIndex = 0;
+    const handleSave = () => {
       modalLoading.value = true;
 
       axios.post("/doc/save", doc.value).then((response) => {
@@ -288,9 +290,6 @@ export default defineComponent({
 
       // 为选择树前面添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-      setTimeout(function (){
-        editor.create()
-      },100);
     };
     /**
      * 新增
@@ -304,9 +303,6 @@ export default defineComponent({
 
       // 为选择树前面添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-      setTimeout(function (){
-        editor.create()
-      },100);
     };
     /**
      * 删除
@@ -338,7 +334,7 @@ export default defineComponent({
 
     onMounted(() => {
       handleQuery();
-
+      editor.create()
     });
 
     return {
@@ -356,7 +352,7 @@ export default defineComponent({
       doc,
       modalVisible,
       modalLoading,
-      handleModalOk,
+      handleSave,
 
       handleDelete,
       treeSelectData,
