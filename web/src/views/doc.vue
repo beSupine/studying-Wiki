599885adb406c5b3fb53cba95cab3aa11e16603d
beSupine <shoufuqiu@outlook.com>
2023-09-25 11:54:34 +0800

    8-12 BUG修复：初始打开文档管理页面，直接新增文档时，会报错：电子书不能为空
    遗留BUG： :defaultSelectedKeys无效

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -3,25 +3,28 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <h3 v-if="level1.length === 0">对不起，找不到相关文档！</h3>
       <a-row>
         <a-col :span="6">
           <a-tree
+
               v-if="level1.length > 0"
               :tree-data="level1"
               @select="onSelect"
               :replaceFields="{title: 'name', key: 'id', value: 'id'}"
               :defaultExpandAll="true"
               :defaultSelectedKeys="defaultSelectedKeys"
+
           >
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div class="wangeditor" :innerHTML="html"></div>
         </a-col>
       </a-row>
     </a-layout-content>
   </a-layout>
 </template>
-
 
 
 <script lang="ts">
@@ -36,9 +39,26 @@ export default defineComponent({
   setup() {
     const route = useRoute()
     const docs = ref();
+    const html = ref();
+    //测出来这个参数无效，不会选中
+    const defaultSelectedKeys = ref();
+    defaultSelectedKeys.value = [];
 
     const level1 = ref();//一级文档树，children属性就是二级文档
     level1.value = [];
+    /**
+     * 内容查询
+     **/
+    const handleQueryContent = (id: number) => {
+      axios.get("/doc/find-content/" + id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
     /**
      * 数据查询
      **/
@@ -50,7 +70,11 @@ export default defineComponent({
           console.log("原始数组：", docs.value);
           level1.value = [];
           level1.value = Tool.array2Tree(docs.value, 0);
-          console.log("树形结构：", level1);
+
+          if (Tool.isNotEmpty(level1)) {
+            defaultSelectedKeys.value = [level1.value[0].id];
+            handleQueryContent(level1.value[0].id);
+          }
 
         } else {
           message.error(data.message);
@@ -59,6 +83,14 @@ export default defineComponent({
     };
 
 
+    const onSelect = (selectedKeys: any, info: any) => {
+      console.log('selected', selectedKeys, info);
+      if (Tool.isNotEmpty(selectedKeys)) {
+        //加载内容
+        handleQueryContent(selectedKeys[0]);
+      }
+    }
+
 
     onMounted(() => {
       handleQuery();
@@ -66,10 +98,74 @@ export default defineComponent({
 
     return {
       level1,
+      html,
+      onSelect,
+      defaultSelectedKeys,
     }
   }
 });
 </script>
+
+
+<style>
+/* wangeditor默认样式*/
+/* table 样式 */
+.wangeditor table {
+  border-top: 1px solid #ccc;
+  border-left: 1px solid #ccc;
+}
+
+.wangeditor table td,
+.wangeditor table th {
+  border-bottom: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+  padding: 3px 5px;
+}
+
+.wangeditor table th {
+  border-bottom: 2px solid #ccc;
+  text-align: center;
+}
+
+/* blockquote 样式 */
+.wangeditor blockquote {
+  display: block;
+  border-left: 8px solid #d0e5f2;
+  padding: 5px 10px;
+  margin: 10px 0;
+  line-height: 1.4;
+  font-size: 100%;
+  background-color: #f1f1f1;
+}
+
+/* code 样式 */
+.wangeditor code {
+  display: inline-block;
+  *display: inline;
+  *zoom: 1;
+  background-color: #f1f1f1;
+  border-radius: 3px;
+  padding: 3px 5px;
+  margin: 0 3px;
+}
+
+.wangeditor pre code {
+  display: block;
+}
+
+/* ul ol 样式 */
+.wangeditor ul, ol {
+  margin: 10px 0 10px 20px;
+}
+
+/* 和antdv p冲突，覆盖掉 */
+.wangeditor blockquote p {
+  font-family: "YouYuan";
+  margin: 20px 10px !important;
+  font-size: 16px !important;
+  font-weight: 600;
+}
+</style>
 
 
 
