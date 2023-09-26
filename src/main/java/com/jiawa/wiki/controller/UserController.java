8c593d468ea93c095d8bf10b19f0037a791ddc39
beSupine <shoufuqiu@outlook.com>
2023-09-26 09:24:15 +0800

    9-9 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/jiawa/wiki/controller/UserController.java
+++ src/main/java/com/jiawa/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jiawa.wiki.req.UserLoginReq;
 import com.jiawa.wiki.req.UserQueryReq;
 import com.jiawa.wiki.req.UserResetPasswordReq;
@@ -9,19 +10,32 @@ import com.jiawa.wiki.resp.PageResp;
 import com.jiawa.wiki.resp.UserLoginResp;
 import com.jiawa.wiki.resp.UserQueryResp;
 import com.jiawa.wiki.service.UserService;
+import com.jiawa.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController//返回字符串
 @RequestMapping("/user")
 //@Controller//返回页面
 public class UserController {
 
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
     @Resource
     private UserService userService;
+    @Resource
+    private SnowFlake snowFlake;
+    @Resource
+    private RedisTemplate redisTemplate;
+
+
     /**
      * GET,POST,PUT,DELETE分别对应查询，新增，修改，删除
      */
@@ -32,6 +46,7 @@ public class UserController {
         resp.setContent(list);
         return resp;
     }
+
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req) {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
@@ -39,12 +54,14 @@ public class UserController {
         userService.save(req);
         return resp;
     }
+
     @DeleteMapping("/delete/{id}")
     public CommonResp delete(@PathVariable Long id) {
         CommonResp resp = new CommonResp<>();
         userService.delete(id);
         return resp;
     }
+
     @PostMapping("/reset-password")
     public CommonResp resetPassword(@Valid @RequestBody UserResetPasswordReq req) {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
@@ -59,6 +76,12 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token:{},并放入redis中",token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp),
+                3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
